0ae32332c3eca7f898e93cec6522a65548d7035d
zheng wang <wangzheng0902@126.com>
2022-10-12 10:39:19 +0800

    day1

---
+++ test.java
@@ -0,0 +1,12 @@
+public class test {
+    public static void main(String[] args){
+        char ch = '呵';
+        System.out.println(ch);
+
+        boolean b = true;
+        System.out.println(b);
+
+        b = false;
+        System.out.println(b);
+    }
+}
